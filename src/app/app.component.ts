4e85eda6243e2142b699e8ae36d5e32c1b0af241
Abdul Samad <ee.abdulsamad@gmail.com>
2024-04-29 01:04:37 +0500

    Fixed config issues

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,24 @@
 import { Component } from '@angular/core';
-// import * as AOS from 'aos';
-// import 'aos/dist/aos.css';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
 
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, HeaderComponent,FooterComponent],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrl: './app.component.css'
 })
+
 export class AppComponent {
 
   title = 'project-ecommerce-store';
 
 
     constructor() {
-      // Initialize AOS here
-      // AOS.init({
-      //   duration: 800,
-      //   easing: 'ease-in-out',
-      //   once: true
-      // });
+
     }
   
 }
